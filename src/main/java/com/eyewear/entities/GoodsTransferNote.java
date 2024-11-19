3d7dcf06b0d110f5adce35163b546589019fc691
ntnhtc <han.ntnhtc@gmail.com>
2024-11-19 16:12:14 +0700

    create entities, service, repository for Transfering Product module

---
+++ src/main/java/com/eyewear/entities/GoodsTransferNote.java
@@ -0,0 +1,53 @@
+package com.eyewear.entities;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "transfer-notes")
+public class GoodsTransferNote {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private Long importBranchId;
+	
+	private Long exportBranchId;
+	
+	private LocalDateTime createdAt; 
+	
+	private LocalDateTime receivedAt;
+	
+	private String status;
+	
+	@OneToMany(mappedBy = "note", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<TransferProduct> products;
+	
+	public void addTransferProduct(TransferProduct product) {
+		this.products.add(product);
+	}
+	
+	public void removeTransferProduct(TransferProduct product) {
+		this.products.remove(product);
+	}
+	
+	public void request(Long productId, Long branchId, int quantity) {
+		this.exportBranchId = branchId;
+		TransferProduct newRequest = new TransferProduct(productId, this.id, quantity);
+		this.products.add(newRequest);
+	}
+}
--- src/main/java/com/eyewear/entities/Product.java
+++ src/main/java/com/eyewear/entities/Product.java
@@ -1,5 +1,38 @@
 package com.eyewear.entities;
 
-public class Product {
+import java.util.List;
 
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "products", uniqueConstraints = @UniqueConstraint(columnNames = "name"))
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(length=100, nullable = false)
+	private String name;
+	
+	@Column(nullable = false)
+	private double price;
+	
+	private String description;
+	
+	private String brand;
+	
+	@OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<BranchProduct> branches;
 }
