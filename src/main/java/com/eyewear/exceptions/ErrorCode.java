23d038242b17299bed241a58c7dc1db221de73f2
nhathuan <huanchaunhat@gmail.com>
2024-11-25 03:59:18 +0700

    JWT va config Authentication cac chuc nang

--- src/main/java/com/eyewear/exceptions/ErrorCode.java
+++ src/main/java/com/eyewear/exceptions/ErrorCode.java
@@ -10,9 +10,11 @@ public enum ErrorCode {
     INVALID_KEY(1000, "Lỗi khai báo sai Message Key."),
     EMAIL_EXISTED(1001, "Email đã được đăng ký trước đó, vui lòng nhập Email khác."),
     EMAIL_INVALID(1002, "Vui lòng nhập đúng email!"),
-    PASSWORD_INVALID(1003, "Mật khẩu phải chứa ít nhất 8 kí tự!")
+    PASSWORD_INVALID(1003, "Mật khẩu phải chứa ít nhất 8 kí tự!"),
+    USER_NOT_EXISTED(1004, "Email đăng nhập không tồn tại hoặc không đúng!"),
+    UNAUTHENTICATED(1004, "Sai email hoặc mật khẩu!")
 
-    ;
+            ;
 
     ErrorCode(int code, String message) {
         this.code = code;
---
+++ src/main/java/com/eyewear/services/AuthenticationService.java
@@ -0,0 +1,14 @@
+package com.eyewear.services;
+
+import com.eyewear.dto.request.AuthenticationRequest;
+import com.eyewear.dto.request.IntrospectRequest;
+import com.eyewear.repositories.AuthenticationResponse;
+import com.eyewear.repositories.IntrospectResponse;
+import com.nimbusds.jose.JOSEException;
+
+import java.text.ParseException;
+
+public interface AuthenticationService {
+    public AuthenticationResponse authenticate(AuthenticationRequest request);
+    public IntrospectResponse introspect(IntrospectRequest request) throws JOSEException, ParseException;
+}
