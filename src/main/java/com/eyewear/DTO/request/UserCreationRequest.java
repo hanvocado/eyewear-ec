247bba950a10b594ec6a0d31a559acc9878ef4e4
PhucVoTrieu <phucvotrieu@gmail.com>
2024-11-28 11:01:57 +0700

    new file

---
+++ src/main/java/com/eyewear/DTO/request/UserCreationRequest.java
@@ -0,0 +1,23 @@
+package com.eyewear.DTO.request;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserCreationRequest {
+    @Email(message = "EMAIL_INVALID")
+    String email;
+
+    @Size(min = 8, message = "PASSWORD_INVALID")
+    String password;
+    String phone;
+    String firstName;
+    String lastName;
+    String address;
+}
