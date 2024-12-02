054fb27425fc7389839656cb78a7a1a1cab84d81
PhucVoTrieu <phucvotrieu@gmail.com>
2024-12-02 17:20:22 +0700

    userUpdateRequest hoàn tác

---
+++ src/main/java/com/eyewear/DTO/request/UserUpdateRequest.java
@@ -0,0 +1,19 @@
+package com.eyewear.DTO.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UserUpdateRequest {
+    String password;
+    String phone;
+    String firstName;
+    String lastName;
+    String address;
+    String picture;
+}
+
