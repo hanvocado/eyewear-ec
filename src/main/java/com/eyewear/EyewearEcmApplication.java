294070d5514ca005cfd9621105bb802cb1cc9520
bin-bard <thanhtai01122004@gmail.com>
2024-12-02 15:37:46 +0700

    .

--- src/main/java/com/eyewear/EyewearEcmApplication.java
+++ src/main/java/com/eyewear/EyewearEcmApplication.java
@@ -2,18 +2,11 @@ package com.eyewear;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EyewearEcmApplication extends SpringBootServletInitializer {
+public class EyewearEcmApplication {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(EyewearEcmApplication.class);
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(EyewearEcmApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(EyewearEcmApplication.class, args);
+	}
 }
