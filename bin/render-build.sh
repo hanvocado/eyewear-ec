4088f7b6dc064cb4739768469a0be9d2bac53cdb
bin-bard <thanhtai01122004@gmail.com>
2024-12-15 23:13:59 +0700

    Add Dockerfile for Render deployment

--- bin/render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-# Build the project
-./mvnw clean install -DskipTests
-
-# Copy the built JAR file to the deployment directory 
-cp target/*.jar app.jar
